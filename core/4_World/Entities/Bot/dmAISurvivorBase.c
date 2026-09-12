3bc529fec54f0851d12eb12d6c283dadd5213594
devalio <github@devalio.ru>
2026-09-12 04:41:06 +0300

    Fight: zombie deals less damage

--- core/4_World/Entities/Bot/dmAISurvivorBase.c
+++ core/4_World/Entities/Bot/dmAISurvivorBase.c
@@ -1090,6 +1090,9 @@ class dmAISurvivorBase : PlayerBase
 			float shock = damageResult.GetDamage("", "Shock");
 			if (shock > 0.0)
 				AddHealth("", "Shock", shock * 0.5);
+			float hlth = damageResult.GetDamage("", "Health");
+			if (hlth > 0.0)
+				AddHealth("", "Health", hlth * 0.5);
 		}
 
 		if (source)
