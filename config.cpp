cde83d054a659ec967bfb4ac52938674c35df6b1
devalio <github@devalio.ru>
2026-09-13 16:55:13 +0300

    Voice: 50 categorized lines (config + mapping + placeholder audio)

--- config.cpp
+++ config.cpp
@@ -345,12 +345,571 @@ class CfgVehicles
 class CfgSoundShaders
 {
 	class baseCharacter_SoundShader;
-	class dmBotVoice_test_SoundShader : baseCharacter_SoundShader
+
+	// greeting
+	class dmBotVoice_dandy_SoundShader : baseCharacter_SoundShader
 	{
 		samples[]=
 		{
 			{
-				"botorama\voices\test",
+				"botorama\voices\greeting\dandy",
+				1
+			}
+		};
+		volume=1;
+	};
+	class dmBotVoice_what_a_fruit_SoundShader : baseCharacter_SoundShader
+	{
+		samples[]=
+		{
+			{
+				"botorama\voices\greeting\what_a_fruit",
+				1
+			}
+		};
+		volume=1;
+	};
+	class dmBotVoice_sour_face_SoundShader : baseCharacter_SoundShader
+	{
+		samples[]=
+		{
+			{
+				"botorama\voices\greeting\sour_face",
+				1
+			}
+		};
+		volume=1;
+	};
+	class dmBotVoice_like_a_bum_SoundShader : baseCharacter_SoundShader
+	{
+		samples[]=
+		{
+			{
+				"botorama\voices\greeting\like_a_bum",
+				1
+			}
+		};
+		volume=1;
+	};
+	class dmBotVoice_life_worn_SoundShader : baseCharacter_SoundShader
+	{
+		samples[]=
+		{
+			{
+				"botorama\voices\greeting\life_worn",
+				1
+			}
+		};
+		volume=1;
+	};
+	class dmBotVoice_familiar_face_SoundShader : baseCharacter_SoundShader
+	{
+		samples[]=
+		{
+			{
+				"botorama\voices\greeting\familiar_face",
+				1
+			}
+		};
+		volume=1;
+	};
+	class dmBotVoice_hey_there_SoundShader : baseCharacter_SoundShader
+	{
+		samples[]=
+		{
+			{
+				"botorama\voices\greeting\hey_there",
+				1
+			}
+		};
+		volume=1;
+	};
+	class dmBotVoice_my_respects_SoundShader : baseCharacter_SoundShader
+	{
+		samples[]=
+		{
+			{
+				"botorama\voices\greeting\my_respects",
+				1
+			}
+		};
+		volume=1;
+	};
+	class dmBotVoice_glad_to_see_SoundShader : baseCharacter_SoundShader
+	{
+		samples[]=
+		{
+			{
+				"botorama\voices\greeting\glad_to_see",
+				1
+			}
+		};
+		volume=1;
+	};
+	class dmBotVoice_what_people_SoundShader : baseCharacter_SoundShader
+	{
+		samples[]=
+		{
+			{
+				"botorama\voices\greeting\what_people",
+				1
+			}
+		};
+		volume=1;
+	};
+
+	// wake
+	class dmBotVoice_why_not_dead_SoundShader : baseCharacter_SoundShader
+	{
+		samples[]=
+		{
+			{
+				"botorama\voices\wake\why_not_dead",
+				1
+			}
+		};
+		volume=1;
+	};
+	class dmBotVoice_it_hurts_SoundShader : baseCharacter_SoundShader
+	{
+		samples[]=
+		{
+			{
+				"botorama\voices\wake\it_hurts",
+				1
+			}
+		};
+		volume=1;
+	};
+	class dmBotVoice_screw_it_all_SoundShader : baseCharacter_SoundShader
+	{
+		samples[]=
+		{
+			{
+				"botorama\voices\wake\screw_it_all",
+				1
+			}
+		};
+		volume=1;
+	};
+	class dmBotVoice_what_happened_SoundShader : baseCharacter_SoundShader
+	{
+		samples[]=
+		{
+			{
+				"botorama\voices\wake\what_happened",
+				1
+			}
+		};
+		volume=1;
+	};
+	class dmBotVoice_screw_everyone_SoundShader : baseCharacter_SoundShader
+	{
+		samples[]=
+		{
+			{
+				"botorama\voices\wake\screw_everyone",
+				1
+			}
+		};
+		volume=1;
+	};
+
+	// passenger
+	class dmBotVoice_best_driver_SoundShader : baseCharacter_SoundShader
+	{
+		samples[]=
+		{
+			{
+				"botorama\voices\passenger\best_driver",
+				1
+			}
+		};
+		volume=1;
+	};
+	class dmBotVoice_not_firewood_SoundShader : baseCharacter_SoundShader
+	{
+		samples[]=
+		{
+			{
+				"botorama\voices\passenger\not_firewood",
+				1
+			}
+		};
+		volume=1;
+	};
+	class dmBotVoice_should_take_bus_SoundShader : baseCharacter_SoundShader
+	{
+		samples[]=
+		{
+			{
+				"botorama\voices\passenger\should_take_bus",
+				1
+			}
+		};
+		volume=1;
+	};
+	class dmBotVoice_that_hurt_SoundShader : baseCharacter_SoundShader
+	{
+		samples[]=
+		{
+			{
+				"botorama\voices\passenger\that_hurt",
+				1
+			}
+		};
+		volume=1;
+	};
+	class dmBotVoice_still_alive_SoundShader : baseCharacter_SoundShader
+	{
+		samples[]=
+		{
+			{
+				"botorama\voices\passenger\still_alive",
+				1
+			}
+		};
+		volume=1;
+	};
+
+	// idle
+	class dmBotVoice_so_sleepy_SoundShader : baseCharacter_SoundShader
+	{
+		samples[]=
+		{
+			{
+				"botorama\voices\idle\so_sleepy",
+				1
+			}
+		};
+		volume=1;
+	};
+	class dmBotVoice_no_money_SoundShader : baseCharacter_SoundShader
+	{
+		samples[]=
+		{
+			{
+				"botorama\voices\idle\no_money",
+				1
+			}
+		};
+		volume=1;
+	};
+	class dmBotVoice_good_days_work_SoundShader : baseCharacter_SoundShader
+	{
+		samples[]=
+		{
+			{
+				"botorama\voices\idle\good_days_work",
+				1
+			}
+		};
+		volume=1;
+	};
+	class dmBotVoice_shoot_someone_SoundShader : baseCharacter_SoundShader
+	{
+		samples[]=
+		{
+			{
+				"botorama\voices\idle\shoot_someone",
+				1
+			}
+		};
+		volume=1;
+	};
+
+	// patrol
+	class dmBotVoice_greedy_bastard_SoundShader : baseCharacter_SoundShader
+	{
+		samples[]=
+		{
+			{
+				"botorama\voices\patrol\greedy_bastard",
+				1
+			}
+		};
+		volume=1;
+	};
+	class dmBotVoice_you_are_the_man_SoundShader : baseCharacter_SoundShader
+	{
+		samples[]=
+		{
+			{
+				"botorama\voices\patrol\you_are_the_man",
+				1
+			}
+		};
+		volume=1;
+	};
+	class dmBotVoice_west_is_fine_SoundShader : baseCharacter_SoundShader
+	{
+		samples[]=
+		{
+			{
+				"botorama\voices\patrol\west_is_fine",
+				1
+			}
+		};
+		volume=1;
+	};
+	class dmBotVoice_shoot_a_boar_SoundShader : baseCharacter_SoundShader
+	{
+		samples[]=
+		{
+			{
+				"botorama\voices\patrol\shoot_a_boar",
+				1
+			}
+		};
+		volume=1;
+	};
+	class dmBotVoice_pissed_himself_SoundShader : baseCharacter_SoundShader
+	{
+		samples[]=
+		{
+			{
+				"botorama\voices\patrol\pissed_himself",
+				1
+			}
+		};
+		volume=1;
+	};
+
+	// aimed_at
+	class dmBotVoice_put_gun_down_SoundShader : baseCharacter_SoundShader
+	{
+		samples[]=
+		{
+			{
+				"botorama\voices\aimed_at\put_gun_down",
+				1
+			}
+		};
+		volume=1;
+	};
+	class dmBotVoice_where_aiming_SoundShader : baseCharacter_SoundShader
+	{
+		samples[]=
+		{
+			{
+				"botorama\voices\aimed_at\where_aiming",
+				1
+			}
+		};
+		volume=1;
+	};
+	class dmBotVoice_you_joking_SoundShader : baseCharacter_SoundShader
+	{
+		samples[]=
+		{
+			{
+				"botorama\voices\aimed_at\you_joking",
+				1
+			}
+		};
+		volume=1;
+	};
+	class dmBotVoice_careful_SoundShader : baseCharacter_SoundShader
+	{
+		samples[]=
+		{
+			{
+				"botorama\voices\aimed_at\careful",
+				1
+			}
+		};
+		volume=1;
+	};
+	class dmBotVoice_never_point_gun_SoundShader : baseCharacter_SoundShader
+	{
+		samples[]=
+		{
+			{
+				"botorama\voices\aimed_at\never_point_gun",
+				1
+			}
+		};
+		volume=1;
+	};
+
+	// heard_shot
+	class dmBotVoice_shooting_SoundShader : baseCharacter_SoundShader
+	{
+		samples[]=
+		{
+			{
+				"botorama\voices\heard_shot\shooting",
+				1
+			}
+		};
+		volume=1;
+	};
+	class dmBotVoice_someone_fun_SoundShader : baseCharacter_SoundShader
+	{
+		samples[]=
+		{
+			{
+				"botorama\voices\heard_shot\someone_fun",
+				1
+			}
+		};
+		volume=1;
+	};
+	class dmBotVoice_they_going_hard_SoundShader : baseCharacter_SoundShader
+	{
+		samples[]=
+		{
+			{
+				"botorama\voices\heard_shot\they_going_hard",
+				1
+			}
+		};
+		volume=1;
+	};
+	class dmBotVoice_action_without_me_SoundShader : baseCharacter_SoundShader
+	{
+		samples[]=
+		{
+			{
+				"botorama\voices\heard_shot\action_without_me",
+				1
+			}
+		};
+		volume=1;
+	};
+	class dmBotVoice_no_rest_SoundShader : baseCharacter_SoundShader
+	{
+		samples[]=
+		{
+			{
+				"botorama\voices\heard_shot\no_rest",
+				1
+			}
+		};
+		volume=1;
+	};
+
+	// got_shot
+	class dmBotVoice_what_a_hit_SoundShader : baseCharacter_SoundShader
+	{
+		samples[]=
+		{
+			{
+				"botorama\voices\got_shot\what_a_hit",
+				1
+			}
+		};
+		volume=1;
+	};
+	class dmBotVoice_on_me_on_me_SoundShader : baseCharacter_SoundShader
+	{
+		samples[]=
+		{
+			{
+				"botorama\voices\got_shot\on_me_on_me",
+				1
+			}
+		};
+		volume=1;
+	};
+	class dmBotVoice_im_shot_SoundShader : baseCharacter_SoundShader
+	{
+		samples[]=
+		{
+			{
+				"botorama\voices\got_shot\im_shot",
+				1
+			}
+		};
+		volume=1;
+	};
+
+	// combat
+	class dmBotVoice_here_we_go_SoundShader : baseCharacter_SoundShader
+	{
+		samples[]=
+		{
+			{
+				"botorama\voices\combat\here_we_go",
+				1
+			}
+		};
+		volume=1;
+	};
+	class dmBotVoice_fun_begins_SoundShader : baseCharacter_SoundShader
+	{
+		samples[]=
+		{
+			{
+				"botorama\voices\combat\fun_begins",
+				1
+			}
+		};
+		volume=1;
+	};
+	class dmBotVoice_contact_SoundShader : baseCharacter_SoundShader
+	{
+		samples[]=
+		{
+			{
+				"botorama\voices\combat\contact",
+				1
+			}
+		};
+		volume=1;
+	};
+	class dmBotVoice_who_else_SoundShader : baseCharacter_SoundShader
+	{
+		samples[]=
+		{
+			{
+				"botorama\voices\combat\who_else",
+				1
+			}
+		};
+		volume=1;
+	};
+	class dmBotVoice_future_corpses_SoundShader : baseCharacter_SoundShader
+	{
+		samples[]=
+		{
+			{
+				"botorama\voices\combat\future_corpses",
+				1
+			}
+		};
+		volume=1;
+	};
+
+	// escort
+	class dmBotVoice_lets_go_ready_SoundShader : baseCharacter_SoundShader
+	{
+		samples[]=
+		{
+			{
+				"botorama\voices\escort\lets_go_ready",
+				1
+			}
+		};
+		volume=1;
+	};
+	class dmBotVoice_anywhere_SoundShader : baseCharacter_SoundShader
+	{
+		samples[]=
+		{
+			{
+				"botorama\voices\escort\anywhere",
+				1
+			}
+		};
+		volume=1;
+	};
+	class dmBotVoice_no_time_to_smoke_SoundShader : baseCharacter_SoundShader
+	{
+		samples[]=
+		{
+			{
+				"botorama\voices\escort\no_time_to_smoke",
 				1
 			}
 		};
@@ -361,11 +920,423 @@ class CfgSoundShaders
 class CfgSoundSets
 {
 	class baseCharacter_SoundSet;
-	class dmBotVoice_test_SoundSet : baseCharacter_SoundSet
+
+	// greeting
+	class dmBotVoice_dandy_SoundSet : baseCharacter_SoundSet
 	{
 		soundShaders[]=
 		{
-			"dmBotVoice_test_SoundShader"
+			"dmBotVoice_dandy_SoundShader"
+		};
+		duration=1.04;
+	};
+	class dmBotVoice_what_a_fruit_SoundSet : baseCharacter_SoundSet
+	{
+		soundShaders[]=
+		{
+			"dmBotVoice_what_a_fruit_SoundShader"
+		};
+		duration=1.04;
+	};
+	class dmBotVoice_sour_face_SoundSet : baseCharacter_SoundSet
+	{
+		soundShaders[]=
+		{
+			"dmBotVoice_sour_face_SoundShader"
+		};
+		duration=1.04;
+	};
+	class dmBotVoice_like_a_bum_SoundSet : baseCharacter_SoundSet
+	{
+		soundShaders[]=
+		{
+			"dmBotVoice_like_a_bum_SoundShader"
+		};
+		duration=1.04;
+	};
+	class dmBotVoice_life_worn_SoundSet : baseCharacter_SoundSet
+	{
+		soundShaders[]=
+		{
+			"dmBotVoice_life_worn_SoundShader"
+		};
+		duration=1.04;
+	};
+	class dmBotVoice_familiar_face_SoundSet : baseCharacter_SoundSet
+	{
+		soundShaders[]=
+		{
+			"dmBotVoice_familiar_face_SoundShader"
+		};
+		duration=1.04;
+	};
+	class dmBotVoice_hey_there_SoundSet : baseCharacter_SoundSet
+	{
+		soundShaders[]=
+		{
+			"dmBotVoice_hey_there_SoundShader"
+		};
+		duration=1.04;
+	};
+	class dmBotVoice_my_respects_SoundSet : baseCharacter_SoundSet
+	{
+		soundShaders[]=
+		{
+			"dmBotVoice_my_respects_SoundShader"
+		};
+		duration=1.04;
+	};
+	class dmBotVoice_glad_to_see_SoundSet : baseCharacter_SoundSet
+	{
+		soundShaders[]=
+		{
+			"dmBotVoice_glad_to_see_SoundShader"
+		};
+		duration=1.04;
+	};
+	class dmBotVoice_what_people_SoundSet : baseCharacter_SoundSet
+	{
+		soundShaders[]=
+		{
+			"dmBotVoice_what_people_SoundShader"
+		};
+		duration=1.04;
+	};
+
+	// wake
+	class dmBotVoice_why_not_dead_SoundSet : baseCharacter_SoundSet
+	{
+		soundShaders[]=
+		{
+			"dmBotVoice_why_not_dead_SoundShader"
+		};
+		duration=1.04;
+	};
+	class dmBotVoice_it_hurts_SoundSet : baseCharacter_SoundSet
+	{
+		soundShaders[]=
+		{
+			"dmBotVoice_it_hurts_SoundShader"
+		};
+		duration=1.04;
+	};
+	class dmBotVoice_screw_it_all_SoundSet : baseCharacter_SoundSet
+	{
+		soundShaders[]=
+		{
+			"dmBotVoice_screw_it_all_SoundShader"
+		};
+		duration=1.04;
+	};
+	class dmBotVoice_what_happened_SoundSet : baseCharacter_SoundSet
+	{
+		soundShaders[]=
+		{
+			"dmBotVoice_what_happened_SoundShader"
+		};
+		duration=1.04;
+	};
+	class dmBotVoice_screw_everyone_SoundSet : baseCharacter_SoundSet
+	{
+		soundShaders[]=
+		{
+			"dmBotVoice_screw_everyone_SoundShader"
+		};
+		duration=1.04;
+	};
+
+	// passenger
+	class dmBotVoice_best_driver_SoundSet : baseCharacter_SoundSet
+	{
+		soundShaders[]=
+		{
+			"dmBotVoice_best_driver_SoundShader"
+		};
+		duration=1.04;
+	};
+	class dmBotVoice_not_firewood_SoundSet : baseCharacter_SoundSet
+	{
+		soundShaders[]=
+		{
+			"dmBotVoice_not_firewood_SoundShader"
+		};
+		duration=1.04;
+	};
+	class dmBotVoice_should_take_bus_SoundSet : baseCharacter_SoundSet
+	{
+		soundShaders[]=
+		{
+			"dmBotVoice_should_take_bus_SoundShader"
+		};
+		duration=1.04;
+	};
+	class dmBotVoice_that_hurt_SoundSet : baseCharacter_SoundSet
+	{
+		soundShaders[]=
+		{
+			"dmBotVoice_that_hurt_SoundShader"
+		};
+		duration=1.04;
+	};
+	class dmBotVoice_still_alive_SoundSet : baseCharacter_SoundSet
+	{
+		soundShaders[]=
+		{
+			"dmBotVoice_still_alive_SoundShader"
+		};
+		duration=1.04;
+	};
+
+	// idle
+	class dmBotVoice_so_sleepy_SoundSet : baseCharacter_SoundSet
+	{
+		soundShaders[]=
+		{
+			"dmBotVoice_so_sleepy_SoundShader"
+		};
+		duration=1.04;
+	};
+	class dmBotVoice_no_money_SoundSet : baseCharacter_SoundSet
+	{
+		soundShaders[]=
+		{
+			"dmBotVoice_no_money_SoundShader"
+		};
+		duration=1.04;
+	};
+	class dmBotVoice_good_days_work_SoundSet : baseCharacter_SoundSet
+	{
+		soundShaders[]=
+		{
+			"dmBotVoice_good_days_work_SoundShader"
+		};
+		duration=1.04;
+	};
+	class dmBotVoice_shoot_someone_SoundSet : baseCharacter_SoundSet
+	{
+		soundShaders[]=
+		{
+			"dmBotVoice_shoot_someone_SoundShader"
+		};
+		duration=1.04;
+	};
+
+	// patrol
+	class dmBotVoice_greedy_bastard_SoundSet : baseCharacter_SoundSet
+	{
+		soundShaders[]=
+		{
+			"dmBotVoice_greedy_bastard_SoundShader"
+		};
+		duration=1.04;
+	};
+	class dmBotVoice_you_are_the_man_SoundSet : baseCharacter_SoundSet
+	{
+		soundShaders[]=
+		{
+			"dmBotVoice_you_are_the_man_SoundShader"
+		};
+		duration=1.04;
+	};
+	class dmBotVoice_west_is_fine_SoundSet : baseCharacter_SoundSet
+	{
+		soundShaders[]=
+		{
+			"dmBotVoice_west_is_fine_SoundShader"
+		};
+		duration=1.04;
+	};
+	class dmBotVoice_shoot_a_boar_SoundSet : baseCharacter_SoundSet
+	{
+		soundShaders[]=
+		{
+			"dmBotVoice_shoot_a_boar_SoundShader"
+		};
+		duration=1.04;
+	};
+	class dmBotVoice_pissed_himself_SoundSet : baseCharacter_SoundSet
+	{
+		soundShaders[]=
+		{
+			"dmBotVoice_pissed_himself_SoundShader"
+		};
+		duration=1.04;
+	};
+
+	// aimed_at
+	class dmBotVoice_put_gun_down_SoundSet : baseCharacter_SoundSet
+	{
+		soundShaders[]=
+		{
+			"dmBotVoice_put_gun_down_SoundShader"
+		};
+		duration=1.04;
+	};
+	class dmBotVoice_where_aiming_SoundSet : baseCharacter_SoundSet
+	{
+		soundShaders[]=
+		{
+			"dmBotVoice_where_aiming_SoundShader"
+		};
+		duration=1.04;
+	};
+	class dmBotVoice_you_joking_SoundSet : baseCharacter_SoundSet
+	{
+		soundShaders[]=
+		{
+			"dmBotVoice_you_joking_SoundShader"
+		};
+		duration=1.04;
+	};
+	class dmBotVoice_careful_SoundSet : baseCharacter_SoundSet
+	{
+		soundShaders[]=
+		{
+			"dmBotVoice_careful_SoundShader"
+		};
+		duration=1.04;
+	};
+	class dmBotVoice_never_point_gun_SoundSet : baseCharacter_SoundSet
+	{
+		soundShaders[]=
+		{
+			"dmBotVoice_never_point_gun_SoundShader"
+		};
+		duration=1.04;
+	};
+
+	// heard_shot
+	class dmBotVoice_shooting_SoundSet : baseCharacter_SoundSet
+	{
+		soundShaders[]=
+		{
+			"dmBotVoice_shooting_SoundShader"
+		};
+		duration=1.04;
+	};
+	class dmBotVoice_someone_fun_SoundSet : baseCharacter_SoundSet
+	{
+		soundShaders[]=
+		{
+			"dmBotVoice_someone_fun_SoundShader"
+		};
+		duration=1.04;
+	};
+	class dmBotVoice_they_going_hard_SoundSet : baseCharacter_SoundSet
+	{
+		soundShaders[]=
+		{
+			"dmBotVoice_they_going_hard_SoundShader"
+		};
+		duration=1.04;
+	};
+	class dmBotVoice_action_without_me_SoundSet : baseCharacter_SoundSet
+	{
+		soundShaders[]=
+		{
+			"dmBotVoice_action_without_me_SoundShader"
+		};
+		duration=1.04;
+	};
+	class dmBotVoice_no_rest_SoundSet : baseCharacter_SoundSet
+	{
+		soundShaders[]=
+		{
+			"dmBotVoice_no_rest_SoundShader"
+		};
+		duration=1.04;
+	};
+
+	// got_shot
+	class dmBotVoice_what_a_hit_SoundSet : baseCharacter_SoundSet
+	{
+		soundShaders[]=
+		{
+			"dmBotVoice_what_a_hit_SoundShader"
+		};
+		duration=1.04;
+	};
+	class dmBotVoice_on_me_on_me_SoundSet : baseCharacter_SoundSet
+	{
+		soundShaders[]=
+		{
+			"dmBotVoice_on_me_on_me_SoundShader"
+		};
+		duration=1.04;
+	};
+	class dmBotVoice_im_shot_SoundSet : baseCharacter_SoundSet
+	{
+		soundShaders[]=
+		{
+			"dmBotVoice_im_shot_SoundShader"
+		};
+		duration=1.04;
+	};
+
+	// combat
+	class dmBotVoice_here_we_go_SoundSet : baseCharacter_SoundSet
+	{
+		soundShaders[]=
+		{
+			"dmBotVoice_here_we_go_SoundShader"
+		};
+		duration=1.04;
+	};
+	class dmBotVoice_fun_begins_SoundSet : baseCharacter_SoundSet
+	{
+		soundShaders[]=
+		{
+			"dmBotVoice_fun_begins_SoundShader"
+		};
+		duration=1.04;
+	};
+	class dmBotVoice_contact_SoundSet : baseCharacter_SoundSet
+	{
+		soundShaders[]=
+		{
+			"dmBotVoice_contact_SoundShader"
+		};
+		duration=1.04;
+	};
+	class dmBotVoice_who_else_SoundSet : baseCharacter_SoundSet
+	{
+		soundShaders[]=
+		{
+			"dmBotVoice_who_else_SoundShader"
+		};
+		duration=1.04;
+	};
+	class dmBotVoice_future_corpses_SoundSet : baseCharacter_SoundSet
+	{
+		soundShaders[]=
+		{
+			"dmBotVoice_future_corpses_SoundShader"
+		};
+		duration=1.04;
+	};
+
+	// escort
+	class dmBotVoice_lets_go_ready_SoundSet : baseCharacter_SoundSet
+	{
+		soundShaders[]=
+		{
+			"dmBotVoice_lets_go_ready_SoundShader"
+		};
+		duration=1.04;
+	};
+	class dmBotVoice_anywhere_SoundSet : baseCharacter_SoundSet
+	{
+		soundShaders[]=
+		{
+			"dmBotVoice_anywhere_SoundShader"
+		};
+		duration=1.04;
+	};
+	class dmBotVoice_no_time_to_smoke_SoundSet : baseCharacter_SoundSet
+	{
+		soundShaders[]=
+		{
+			"dmBotVoice_no_time_to_smoke_SoundShader"
 		};
 		duration=1.04;
 	};
--- core/4_World/Entities/Bot/Voice/dmBotVoice.c
+++ core/4_World/Entities/Bot/Voice/dmBotVoice.c
@@ -1,21 +1,131 @@
-//! dmBotVoice — маппинг голосовых реплик бота: id -> имя CfgSoundSets-звукового
-//! сета (см. config.cpp) и длительность анимации рта.
+//! dmBotVoice — категоризатор голосовых реплик: категории, количество реплик в
+//! категории и маппинг lineId -> имя CfgSoundSets-сета. Звуки описаны в config.cpp,
+//! файлы лежат в voices/<category>/<keyword>.ogg (плейсхолдеры до студии).
+enum dmVoiceCategory
+{
+	GREETING,        // Приветствия
+	WAKE,            // очнулся после отключки
+	PASSENGER_CRASH, // пассажир, водитель врезается
+	IDLE,            // покой/скука
+	PATROL,          // патруль
+	AIMED_AT,        // целятся, но не стреляют
+	HEARD_SHOT,      // услышал выстрел
+	GOT_SHOT,        // получил пулю
+	COMBAT,          // враги, бой
+	ESCORT,          // сопровождение
+	DM_VOICE_CATEGORY_COUNT
+};
+
 class dmBotVoice
 {
-	//! Имя CfgSoundSets-сета реплики. Пустая строка = нет реплики.
-	static string GetSoundSetName(int lineId)
+	//! Имя папки/категории (для логов).
+	static string GetCategoryName(dmVoiceCategory cat)
 	{
-		switch (lineId)
+		switch (cat)
 		{
-		case dmVoiceLine.DM_VOICE_TEST:
-			return "dmBotVoice_test_SoundSet";
+		case GREETING:        return "greeting";
+		case WAKE:            return "wake";
+		case PASSENGER_CRASH: return "passenger";
+		case IDLE:            return "idle";
+		case PATROL:          return "patrol";
+		case AIMED_AT:        return "aimed_at";
+		case HEARD_SHOT:      return "heard_shot";
+		case GOT_SHOT:        return "got_shot";
+		case COMBAT:          return "combat";
+		case ESCORT:          return "escort";
 		}
 		return "";
 	}
 
-	//! Длительность анимации рта (сек) — читается из конфига
-	//! (CfgSoundSets <set> duration, задано рядом со звуком в config.cpp).
-	//! Fallback 1.0, если поле не задано.
+	//! Число реплик в категории.
+	static int GetCategoryCount(dmVoiceCategory cat)
+	{
+		switch (cat)
+		{
+		case GREETING:        return 10;
+		case WAKE:            return 5;
+		case PASSENGER_CRASH: return 5;
+		case IDLE:            return 4;
+		case PATROL:          return 5;
+		case AIMED_AT:        return 5;
+		case HEARD_SHOT:      return 5;
+		case GOT_SHOT:        return 3;
+		case COMBAT:          return 5;
+		case ESCORT:          return 3;
+		}
+		return 0;
+	}
+
+	//! lineId = category*100 + index (index 1..count). Пустая строка = нет реплики.
+	static string GetSoundSetName(int lineId)
+	{
+		switch (lineId)
+		{
+		// greeting
+		case 1:  return "dmBotVoice_dandy_SoundSet";
+		case 2:  return "dmBotVoice_what_a_fruit_SoundSet";
+		case 3:  return "dmBotVoice_sour_face_SoundSet";
+		case 4:  return "dmBotVoice_like_a_bum_SoundSet";
+		case 5:  return "dmBotVoice_life_worn_SoundSet";
+		case 6:  return "dmBotVoice_familiar_face_SoundSet";
+		case 7:  return "dmBotVoice_hey_there_SoundSet";
+		case 8:  return "dmBotVoice_my_respects_SoundSet";
+		case 9:  return "dmBotVoice_glad_to_see_SoundSet";
+		case 10: return "dmBotVoice_what_people_SoundSet";
+		// wake
+		case 101: return "dmBotVoice_why_not_dead_SoundSet";
+		case 102: return "dmBotVoice_it_hurts_SoundSet";
+		case 103: return "dmBotVoice_screw_it_all_SoundSet";
+		case 104: return "dmBotVoice_what_happened_SoundSet";
+		case 105: return "dmBotVoice_screw_everyone_SoundSet";
+		// passenger
+		case 201: return "dmBotVoice_best_driver_SoundSet";
+		case 202: return "dmBotVoice_not_firewood_SoundSet";
+		case 203: return "dmBotVoice_should_take_bus_SoundSet";
+		case 204: return "dmBotVoice_that_hurt_SoundSet";
+		case 205: return "dmBotVoice_still_alive_SoundSet";
+		// idle
+		case 301: return "dmBotVoice_so_sleepy_SoundSet";
+		case 302: return "dmBotVoice_no_money_SoundSet";
+		case 303: return "dmBotVoice_good_days_work_SoundSet";
+		case 304: return "dmBotVoice_shoot_someone_SoundSet";
+		// patrol
+		case 401: return "dmBotVoice_greedy_bastard_SoundSet";
+		case 402: return "dmBotVoice_you_are_the_man_SoundSet";
+		case 403: return "dmBotVoice_west_is_fine_SoundSet";
+		case 404: return "dmBotVoice_shoot_a_boar_SoundSet";
+		case 405: return "dmBotVoice_pissed_himself_SoundSet";
+		// aimed_at
+		case 501: return "dmBotVoice_put_gun_down_SoundSet";
+		case 502: return "dmBotVoice_where_aiming_SoundSet";
+		case 503: return "dmBotVoice_you_joking_SoundSet";
+		case 504: return "dmBotVoice_careful_SoundSet";
+		case 505: return "dmBotVoice_never_point_gun_SoundSet";
+		// heard_shot
+		case 601: return "dmBotVoice_shooting_SoundSet";
+		case 602: return "dmBotVoice_someone_fun_SoundSet";
+		case 603: return "dmBotVoice_they_going_hard_SoundSet";
+		case 604: return "dmBotVoice_action_without_me_SoundSet";
+		case 605: return "dmBotVoice_no_rest_SoundSet";
+		// got_shot
+		case 701: return "dmBotVoice_what_a_hit_SoundSet";
+		case 702: return "dmBotVoice_on_me_on_me_SoundSet";
+		case 703: return "dmBotVoice_im_shot_SoundSet";
+		// combat
+		case 801: return "dmBotVoice_here_we_go_SoundSet";
+		case 802: return "dmBotVoice_fun_begins_SoundSet";
+		case 803: return "dmBotVoice_contact_SoundSet";
+		case 804: return "dmBotVoice_who_else_SoundSet";
+		case 805: return "dmBotVoice_future_corpses_SoundSet";
+		// escort
+		case 901: return "dmBotVoice_lets_go_ready_SoundSet";
+		case 902: return "dmBotVoice_anywhere_SoundSet";
+		case 903: return "dmBotVoice_no_time_to_smoke_SoundSet";
+		}
+		return "";
+	}
+
+	//! Длительность рта (сек) — из конфига CfgSoundSets <set> duration. Fallback 1.0.
 	static float GetTalkDuration(int lineId)
 	{
 		string name = GetSoundSetName(lineId);
